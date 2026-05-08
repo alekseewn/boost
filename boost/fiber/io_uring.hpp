d78b1af7c09e2fb451fa17b8ffef10d44ba758e5
Alekseev Nikita <alekseev.n@selectel.ru>
2026-05-08 21:54:30 +0300

    add pread pwrite

--- boost/fiber/io_uring.hpp
+++ boost/fiber/io_uring.hpp
@@ -63,6 +63,46 @@ int write(int fd, void const * buf, unsigned nbytes, __u64 offset = 0) {
     return op.result;
 }
 
+inline
+int pread(int fd, void * buf, unsigned nbytes, __u64 offset) {
+    algo::work_stealing * algo = algo::work_stealing::current();
+    BOOST_ASSERT_MSG(algo, "work_stealing not active on this thread");
+
+    algo::work_stealing::iouring_op op;
+    op.ctx = context::active();
+    op.result = 0;
+
+    io_uring_sqe * sqe = io_uring_get_sqe(&algo->get_ring());
+    BOOST_ASSERT_MSG(sqe, "io_uring SQ full");
+    io_uring_prep_read(sqe, fd, buf, nbytes, offset);
+    sqe->user_data = reinterpret_cast<__u64>(&op);
+
+    io_uring_submit(&algo->get_ring());
+    context::active()->suspend();
+
+    return op.result;
+}
+
+inline
+int pwrite(int fd, void const * buf, unsigned nbytes, __u64 offset) {
+    algo::work_stealing * algo = algo::work_stealing::current();
+    BOOST_ASSERT_MSG(algo, "work_stealing not active on this thread");
+
+    algo::work_stealing::iouring_op op;
+    op.ctx = context::active();
+    op.result = 0;
+
+    io_uring_sqe * sqe = io_uring_get_sqe(&algo->get_ring());
+    BOOST_ASSERT_MSG(sqe, "io_uring SQ full");
+    io_uring_prep_write(sqe, fd, buf, nbytes, offset);
+    sqe->user_data = reinterpret_cast<__u64>(&op);
+
+    io_uring_submit(&algo->get_ring());
+    context::active()->suspend();
+
+    return op.result;
+}
+
 }}}
 
 #ifdef BOOST_HAS_ABI_HEADERS
--- examples/main.cpp
+++ examples/main.cpp
@@ -9,32 +9,30 @@
 #include <boost/fiber/io_uring.hpp>
 
 void fiber_task() {
-    int fd = open("/tmp/boost_fiber_test", O_RDONLY);
+    int fd = open("/tmp/boost_fiber_test", O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (fd < 0) { perror("open"); return; }
 
-    char buf[256] = {};
-    int n = boost::fibers::io_uring::read(fd, buf, sizeof(buf));
-    std::cerr << "read returned " << n << " bytes: " << buf << std::endl;
+    const char * msg1 = "hello from pwrite!";
+    int rc = boost::fibers::io_uring::pwrite(fd, msg1, std::strlen(msg1), 0);
+    std::cerr << "pwrite returned " << rc << std::endl;
     close(fd);
+
+    fd = open("/tmp/boost_fiber_test", O_RDONLY);
+    if (fd < 0) { perror("open"); return; }
+
+    char buf[64] = {};
+    int n = boost::fibers::io_uring::pread(fd, buf, sizeof(buf), 0);
+    std::cerr << "pread returned " << n << " bytes: " << buf << std::endl;
+    close(fd);
+
+    unlink("/tmp/boost_fiber_test");
 }
 
 int main() {
-    int fd = open("/tmp/boost_fiber_test", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    const char * msg = "Hello from io_uring in Boost.Fiber!";
-    write(fd, msg, std::strlen(msg));
-    close(fd);
-
     boost::fibers::use_scheduling_algorithm<
         boost::fibers::algo::work_stealing>(1);
 
     boost::fibers::fiber f(fiber_task);
-    boost::fibers::fiber g([](){
-        int fn = open("test_file", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-        boost::fibers::io_uring::write(fn, "hello!!!!", 9);
-    });
-    g.join();
     f.join();
-
-    unlink("/tmp/boost_fiber_test");
     return 0;
 }
